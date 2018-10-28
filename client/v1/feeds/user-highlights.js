561c4d6e6879105bcbc3547c3e75d0afba75ce19
Amir <shoqer@gmail.com>
2018-10-28 17:27:17 +0200

    Add highlights and recent activity
    Fix story feed

--- client/v1/feeds/user-highlights.js
+++ client/v1/feeds/user-highlights.js
@@ -1,23 +1,23 @@
 var _ = require('lodash');
 var util = require('util');
 var FeedBase = require('./feed-base');
-var Media = require('../media');
 var Request = require('../request');
+var Media = require('../media');
 
-function TaggedFeed(session, accountId, limit) {
+function UserHighlights(session, accountId, limit) {
   this.session = session;
   this.accountId = accountId;
   this.limit = parseInt(limit) || null;
   FeedBase.apply(this, arguments);
 }
 
-util.inherits(TaggedFeed, FeedBase);
+util.inherits(UserHighlights, FeedBase);
 
-TaggedFeed.prototype.get = function () {
+UserHighlights.prototype.get = function () {
   var that = this;
   return new Request(that.session)
     .setMethod('GET')
-    .setResource('taggedFeed', {
+    .setResource('userHighlights', {
       id: that.accountId,
       maxId: that.getCursor()
     })
@@ -25,12 +25,13 @@ TaggedFeed.prototype.get = function () {
     .then(function(data) {
       var nextMaxId = data.next_max_id ? data.next_max_id.toString() : data.next_max_id;
       that.moreAvailable = data.more_available && !!nextMaxId;
-      if (that.moreAvailable)
+      if (that.moreAvailable) {
         that.setCursor(nextMaxId);
-      return _.map(data.items, function (medium) {
+      }
+      return _.map(data.tray, function (medium) {
         return new Media(that.session, medium);
       });
-    })
+    });
 };
 
-module.exports = TaggedFeed;
+module.exports = UserHighlights;
